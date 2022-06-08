4536dfc2c1eb22e06d34fba6d9fa822f6d7cc6e4
maria-moroz <mma.moroz.mariia@gmail.com>
2022-06-08 15:28:56 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import s from './Filter.module.css';
 
-export default function Filter({ handleFilterChange }) {
+export default function Filter({ handleFilterChange, filter }) {
   const filterId = nanoid();
 
   return (
@@ -15,6 +15,7 @@ export default function Filter({ handleFilterChange }) {
         name="name"
         id={filterId}
         className={s.input}
+        value={filter}
         onChange={handleFilterChange}
       />
     </div>
@@ -22,5 +23,6 @@ export default function Filter({ handleFilterChange }) {
 }
 
 Filter.propTypes = {
+  filter: PropTypes.string.isRequired,
   handleFilterChange: PropTypes.func.isRequired,
 };
